208aaa71ba7bd4ca06727ed009cdc2cce306ec47
Maplewing <sinmaplewing@gmail.com>
2018-04-21 15:13:24 +0800

    Add template support.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
+
+use std::collections::HashMap;
+use rocket_contrib::Template;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-    }
+fn index() -> Template {
+    let mut context = HashMap::new();
+    context.insert("title", "Rust GuestBook");
+    context.insert("body", "Welcome to my guestbook.");
+    Template::render("index", context)
+}
 
-    fn main() {
-        rocket::ignite().mount("/", routes![index]).launch();
-	}
+fn main() {
+    rocket::ignite()
+      .mount("/", routes![index])
+      .attach(Template::fairing())
+      .launch();
+}
