c291d57e3c25f134c184ac37cfa59a393b72df75
Maplewing <sinmaplewing@gmail.com>
2018-04-21 14:42:38 +0800

    Rocket Environment Builded.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-}
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+    }
+
+    fn main() {
+        rocket::ignite().mount("/", routes![index]).launch();
+	}
